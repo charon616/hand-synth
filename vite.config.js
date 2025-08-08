d49de1615dcddd907c0b831fa7717961e6e109ad
charon616 <milkyway.k16@gmail.com>
2025-08-08 21:58:43 +0100

    save

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    allowedHosts: ['c69a39a4f952.ngrok-free.app'],
+  },
   build: {
     target: 'es2022', // Set the target environment to support top-level await
   },
